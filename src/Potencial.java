6c64bfc4b2bed5976c4a9459fae328e775c5fb00
Antonio <antoniolozano.saladillo@gmail.com>
2020-10-02 18:13:46 +0200

    Commit 2
    Añadido que salga desde elevado a 0, ya que lo tenia desde 1

--- src/Potencial.java
+++ src/Potencial.java
@@ -8,7 +8,7 @@ class Potencial extends Thread {
 
     @Override
     public void run() {
-        for (int i = 1; i <= 10; i++) {
+        for (int i = 0; i <= 10; i++) {
             double potencials = Math.pow(number,i);
             System.out.printf("%s: %d ^ %d = %.0f\n", getName(), number, i, potencials); //Pongo %.0f para que no me muestre los decimales.
         }
